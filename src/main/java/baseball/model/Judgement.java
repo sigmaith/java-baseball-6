c5894e61be9fc1443c028cccc679293aaca7588b
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-03 17:33:04 +0900

    feat(Judgement): Judgement 클래스 생성 및 스트라이크 개수 판단 기능 구현

---
+++ src/main/java/baseball/model/Judgement.java
@@ -0,0 +1,27 @@
+package baseball.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Judgement {
+    private int strikeCnt;
+    private int ballCnt;
+
+    public Judgement() {
+        strikeCnt = 0;
+        ballCnt = 0;
+    }
+
+    public void judgeStrike(User user, Computer computer){
+        setStrikeCntZero();
+        List<Integer> userNumber = user.getUserInputNumber();
+        List<Integer> computerNumber = computer.getRandomNumber();
+        for (int i = 0; i < 3; i++) {
+            if (Objects.equals(userNumber.get(i), computerNumber.get(i))) strikeCnt++;
+        }
+    }
+
+    private void setStrikeCntZero(){
+        strikeCnt = 0;
+    }
+}
