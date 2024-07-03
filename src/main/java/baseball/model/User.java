941b998fbfe94a0c414cb66ffac5405749d0dfbb
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-03 15:56:40 +0900

    feat(gameController): GameController 클래스 생성 및 InputView에서의 입력값을 user인스턴스에 넘겨주기

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -7,11 +7,11 @@ public class User {
     private static final int INPUT_NUMBER_SIZE = 3;
     private List<Integer> userInputNumber;
 
-    User(){
+    public User(){
         userInputNumber = new ArrayList<>();
     }
 
-    public void setInputNumber(String inputNumber){
+    public void setUserInputNumber(String inputNumber){
         userInputNumber.clear();
 
         for (int i = 0; i < INPUT_NUMBER_SIZE; i++){
