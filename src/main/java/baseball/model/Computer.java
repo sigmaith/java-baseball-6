04b421f60bd6cbdc2d15371c165cef17d4607776
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-04 20:51:06 +0900

    feat(gameController): 정답 생성 메서드, 입력받은 수를 user에 setter 메서드로 지정

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Computer {
     private List<Integer> randomNumber;
 
-    Computer(){
+    public Computer(){
         randomNumber = new ArrayList<>();
     }
 
