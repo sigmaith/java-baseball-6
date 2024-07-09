50b4d8b8f28ff563bde5a3848ac4057b7726bc0c
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-09 16:04:39 +0900

    feat(outputView): 스트라이크와 볼의 개수 혹은 낫싱 문구 출력 메서드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,9 @@ public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -13,7 +16,19 @@ public class OutputView {
         System.out.println(GAME_END_MESSAGE);
     }
 
-    public static void printGameRestartOrEnd(){
+    public static void printGameRestartOrEnd() {
         System.out.println(GAME_RESTART_OR_END_MESSAGE);
     }
+
+    public static void printBallAndStrikeOrNothing(int ballCnt, int strikeCnt) {
+        if (ballCnt == 0 && strikeCnt == 0) {
+            System.out.println(NOTHING);
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        if (ballCnt != 0) sb.append(ballCnt).append(BALL).append(" ");
+        if (strikeCnt != 0) sb.append(strikeCnt).append(STRIKE);
+        System.out.println(sb);
+    }
+
 }
