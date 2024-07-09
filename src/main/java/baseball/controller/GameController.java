09c5134cc7c07b9d2546a425245c42b5bfc8b326
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-09 16:34:52 +0900

    feat(gameController): 한번의 게임 루틴 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,18 +4,26 @@ import baseball.model.Computer;
 import baseball.model.Judgement;
 import baseball.model.User;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class GameController {
     User user = new User();
     Computer computer = new Computer();
     Judgement judgement = new Judgement();
 
-    public void gameStart(){
+    public void gameStart() {
+        OutputView.printGameStartMessage();
         computer.generateRandomNumber();
 
-        while (true){
+        while (judgement.getStrikeCnt() != 3) {
             String userInput = InputView.getUserInput();
             user.setUserInputNumber(userInput);
+
+            if (judgement.judgeNothing(user, computer)) continue;
+            else OutputView.printBallAndStrikeOrNothing(judgement.getBallCnt(), judgement.getStrikeCnt());
         }
+
+        OutputView.printGameEndMessage();
+
     }
 }
