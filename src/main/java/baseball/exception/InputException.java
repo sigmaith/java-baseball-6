e37a7a9c541c23bd0241edbe670ae12b3e3a0d79
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-11 19:27:17 +0900

    refactor(inputException): 의미있는 변수이름, 메서드를 가지도록 수정

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -1,19 +1,19 @@
 package baseball.exception;
 
 public class InputException {
-    private static final int USER_INPUT_SIZE = 3;
+    private static final int EXPECTED_USER_INPUT_SIZE = 3;
 
-    public static String isValidInput(String userInput) {
-        if (!userInput.matches("[1-9]{3}")) {
+    public static String isExpectedNumberOfUserValid(String expectedNumberofUser) {
+        if (!expectedNumberofUser.matches("[1-9]{3}")) {
             throw new IllegalArgumentException();
         }
-        if (userInput.chars().distinct().count() != USER_INPUT_SIZE) {
+        if (expectedNumberofUser.chars().distinct().count() != EXPECTED_USER_INPUT_SIZE) {
             throw new IllegalArgumentException();
         }
-        return userInput;
+        return expectedNumberofUser;
     }
 
-    public static String isOneOrTwo(String userInput) {
+    public static String isValueOfRestartGameOrNotValid(String userInput) {
         if (!userInput.matches("[1-2]{1}")) throw new IllegalArgumentException();
         return userInput;
     }
