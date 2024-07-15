1dffe58512f093eb7f1432a69cef8e5ac39c2ed9
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-15 17:32:18 +0900

    test(judgement): 볼, 스트라이크 개수 및 낫싱 여부 판단 테스트 작성

---
+++ src/test/java/baseball/model/JudgementTest.java
@@ -0,0 +1,96 @@
+package baseball.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JudgementTest {
+    @Test
+    void 낫싱인지_아닌지_판단_1() {
+        User user = new User();
+        Computer computer = new Computer();
+        Judgement judgement = new Judgement();
+        List<Integer> randomNumber = new ArrayList<>(Arrays.asList(5, 6, 7));
+        computer.setRandomNumber(randomNumber);
+        user.setUserInputNumber("123");
+        assertThat(judgement.isNothing(user, computer)).isEqualTo(true);
+    }
+
+    @Test
+    void 낫싱인지_아닌지_판단_2() {
+        User user = new User();
+        Computer computer = new Computer();
+        Judgement judgement = new Judgement();
+        List<Integer> randomNumber = new ArrayList<>(Arrays.asList(2, 4, 5));
+
+        computer.setRandomNumber(randomNumber);
+        user.setUserInputNumber("123");
+
+        assertThat(judgement.isNothing(user, computer)).isEqualTo(false);
+    }
+
+    @Test
+    void 낫싱이_아닐때_볼과_스트라이크_개수_판단_1() {
+        User user = new User();
+        Computer computer = new Computer();
+        Judgement judgement = new Judgement();
+        List<Integer> randomNumber = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+        computer.setRandomNumber(randomNumber);
+        user.setUserInputNumber("315");
+        judgement.isNothing(user, computer);
+
+        assertThat(judgement.getStrikeCnt()).isEqualTo(0);
+        assertThat(judgement.getBallCnt()).isEqualTo(2);
+    }
+
+    @Test
+    void 낫싱이_아닐때_볼과_스트라이크_개수_판단_2() {
+        User user = new User();
+        Computer computer = new Computer();
+        Judgement judgement = new Judgement();
+        List<Integer> randomNumber = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+        computer.setRandomNumber(randomNumber);
+        user.setUserInputNumber("125");
+        judgement.isNothing(user, computer);
+
+        assertThat(judgement.getStrikeCnt()).isEqualTo(2);
+        assertThat(judgement.getBallCnt()).isEqualTo(0);
+    }
+
+    @Test
+    void 낫싱이_아닐때_볼과_스트라이크_개수_판단_3() {
+        User user = new User();
+        Computer computer = new Computer();
+        Judgement judgement = new Judgement();
+        List<Integer> randomNumber = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+        computer.setRandomNumber(randomNumber);
+        user.setUserInputNumber("321");
+        judgement.isNothing(user, computer);
+
+        assertThat(judgement.getStrikeCnt()).isEqualTo(1);
+        assertThat(judgement.getBallCnt()).isEqualTo(2);
+    }
+
+    @Test
+    void 낫싱이_아닐때_볼과_스트라이크_개수_판단_4() {
+        User user = new User();
+        Computer computer = new Computer();
+        Judgement judgement = new Judgement();
+        List<Integer> randomNumber = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+        computer.setRandomNumber(randomNumber);
+        user.setUserInputNumber("123");
+        judgement.isNothing(user, computer);
+
+        assertThat(judgement.getStrikeCnt()).isEqualTo(3);
+        assertThat(judgement.getBallCnt()).isEqualTo(0);
+    }
+}
